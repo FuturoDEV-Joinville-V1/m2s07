284f5f2cfb49cdd0f173bfc2ff53045c9c992fef
Your Name <you@example.com>
2025-07-04 20:23:03 -0300

    [A03] Atividade 1 - parte 2

--- aula_3/atividade_1_classes.js
+++ aula_3/atividade_1_classes.js
@@ -4,9 +4,33 @@ class Personagem {
     #baseAtaque;
     #baseDefesa;
 
+    constructor(nivel, vida, baseAtaque, baseDefesa) {
+        this.#nivel = nivel;
+        this.#vida = vida;
+        this.#baseAtaque = baseAtaque;
+        this.#baseDefesa = baseDefesa;
+    }
+
     atacar() {}
     defender() {}
     getVida() {}
     setVida() {}
 }
 
+export class Mago extends Personagem {
+    constructor(nivel) {
+        super(nivel, 85 * nivel, 8, 3);
+    }
+}
+
+export class Guerreiro extends Personagem {
+    constructor(nivel) {
+        super(nivel, 105 * nivel, 6, 5);
+    }
+}
+
+export  class Arqueiro extends Personagem {
+    constructor(nivel) {
+        super(nivel, 90 * nivel, 7, 4);
+    }
+}
