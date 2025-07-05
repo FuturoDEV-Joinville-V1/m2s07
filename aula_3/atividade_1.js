c9c7d5754b41a1fd1aa8835012849f1a33bb3863
Your Name <you@example.com>
2025-07-04 21:11:17 -0300

    [A03] Atividade 1 - parte 3

---
+++ aula_3/atividade_1.js
@@ -0,0 +1,25 @@
+import { Mago, Guerreiro, Arqueiro} from './atividade_1_classes.js';
+
+const escolhaUsuario = prompt('Escolha a sua classe [Mago, Guerreiro, Arqueiro]:');
+const escolhaAdversario = prompt('Escolha a sua classe do adversário [Mago, Guerreiro, Arqueiro]:');
+const escolhaNivel = prompt('Escolha o nível dos personagens [1 a 100]:');
+
+let personagemUsuario;
+let personagemAdversario;
+
+if (escolhaUsuario.toLowerCase() === 'mago') {
+    personagemUsuario = new Mago(escolhaNivel);
+} else if (escolhaUsuario.toLowerCase() === 'guerreiro') {
+    personagemUsuario = new Guerreiro(escolhaNivel);
+} else if (escolhaUsuario.toLowerCase() === 'arqueiro') {
+    personagemUsuario = new Arqueiro(escolhaNivel);
+}
+
+if (escolhaAdversario.toLowerCase() === 'mago') {
+    personagemAdversario = new Mago(escolhaNivel);
+} else if (escolhaAdversario.toLowerCase() === 'guerreiro') {
+    personagemAdversario = new Guerreiro(escolhaNivel);
+} else if (escolhaAdversario.toLowerCase() === 'arqueiro') {
+    personagemAdversario = new Arqueiro(escolhaNivel);
+}
+
